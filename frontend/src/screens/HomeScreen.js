fa42ea390d8f31ece4e1fe599b63da819b240181
xlakostax <konstantin.veselovskii@gmail.com>
2021-03-04 00:28:20 +0200

    Improved view of cards with goods

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -18,8 +18,8 @@ const HomeScreens = () => {
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map(product => 
-          <Col sm={12} md={6} lg={4} xl={3}>
+        {products.map((product, index) => 
+          <Col sm={12} md={6} lg={4} xl={3} key={index}>
             <Product product={product} />
           </Col>
         )}
